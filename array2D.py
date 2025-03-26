16f2eda13802a51af824b2832d3e5a7721b0ea5b
Fernando Canchola Cruz <fernando.canchola@encora.com>
2025-03-26 10:18:01 -0600

    Create array2D.py
    Solution for Convert an Array Into a 2D Array With Conditions.

---
+++ array2D.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def findMatrix(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        vacia=[]
+        listaF=[]
+        k=0
+        for i in nums:
+            vacia.append(nums.count(i))
+        mayor=max(vacia)
+        matriz = [[] for _ in range(mayor)]
+        for i in nums:
+            for k in range (0, mayor):
+                if i not in matriz[k]:
+                    matriz[k].append(i)
+                    break
+
+        return matriz
+        
+
+        print(matriz)
+        
